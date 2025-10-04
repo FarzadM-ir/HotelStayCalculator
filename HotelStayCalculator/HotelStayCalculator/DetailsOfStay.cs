d61104d72f4e456429cfa094146b3d7b6bc3554d
Farzad Mozhdekanlou <lordfrzh@gmail.com>
2025-10-04 20:45:00 +0330

    Add files via upload

---
+++ HotelStayCalculator/HotelStayCalculator/DetailsOfStay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelStayCalculator
+{
+    public class DetailsOfStay
+    {
+        public void Details(out int theAmountOfPeople, out int theAmountOfNights)
+        {
+            Console.WriteLine("How many people will be stayin in your room in total (Including yourself):");
+            theAmountOfPeople = Convert.ToInt32(Console.ReadLine());
+            while (theAmountOfPeople <= 0 || theAmountOfPeople > 5)
+            {
+                Console.WriteLine("Please enter a valid number (must be between 1 and 5):");
+                theAmountOfPeople = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("How many nights will your stay be:");
+            theAmountOfNights = Convert.ToInt32(Console.ReadLine());
+            while (theAmountOfNights <= 0 || theAmountOfNights > 30)
+            {
+                Console.WriteLine("Please enter a valid number (must be between 1 and 30):");
+                theAmountOfNights = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
